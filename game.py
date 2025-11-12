210e3f0b35e8114f3eaeb9ae9c19e7a373f25bce
dhart6116 <dhart6116@floridapoly.edu>
2025-11-12 12:13:37 -0500

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
 print("Welcome to Rock-Paper-Scissors!")
-player_choice = input("Choose rock, paper, or scissors: ")
-print(f"You chose: {player_choice}")
+
+import random
+choice = random.randint(1, 3)
+if (choice == 1):
+    strchoice = "rock"
+elif (choice == 2):
+    strchoice = "paper"
+elif (choice == 3):
+    strchoice = "scissors"
+
+print(f"Computer chose: {strchoice}")
